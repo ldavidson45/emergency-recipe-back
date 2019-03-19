776ab5dfb695d2f98ef31e8d9ef98633e32a9042
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-03-19 15:49:24 -0400

    added a few seed items

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,4 @@
 const Recipe = require("../models/Recipe");
-const Comment = require("../models/Comment");
 
 Recipe.remove({}).then(function() {
   Recipe.create({
@@ -15,7 +14,26 @@ Recipe.remove({}).then(function() {
 });
 
 Recipe.create({
-  title: "tacos",
+  title: "pizza",
+  keyIngredients: [
+    "tomato",
+    "mozzarella cheese",
+    "peppers",
+    "onions",
+    "olives",
+    "flour"
+  ],
+  servings: 4,
+  prepTime: "25 minutes",
+  picture:
+    "https://images.unsplash.com/photo-1528490060256-c345efae4442?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80",
+  instructions:
+    "toss & roll the dough, spread your sauce, sprinkle on the cheese, layer on your toppings",
+  isApproved: true
+});
+
+Recipe.create({
+  title: "pasta",
   keyIngredients: ["onion", "tomato", "beef", "peppers"],
   servings: 3,
   prepTime: "20 minutes",
@@ -24,11 +42,3 @@ Recipe.create({
   instructions: "wash, slice and mix all the ingredients",
   isApproved: true
 });
-});
-
-
-
-
-
-
-
