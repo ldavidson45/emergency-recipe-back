e5d8feaacde56920ee269a9eb7d106e92292a761
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-03-19 13:57:26 -0400

    added comment to index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const parser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const recipeController = require("./controllers/recipe.js");
+const commentController = require("./controllers/comment.js");
 
 app.use(cors());
 app.use(parser.json());
 
 app.use("/api/recipe", recipeController);
+app.use("/api/comment", commentController);
 
 app.set("port", process.env.PORT || 3001);
 
