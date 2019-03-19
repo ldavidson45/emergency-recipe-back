725f097f3f04d760dfd378b8875baf7e297beb94
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-03-19 15:41:14 -0400

    adding some more recipes

--- db/seed.js
+++ db/seed.js
@@ -12,3 +12,22 @@ Recipe.remove({}).then(function() {
     isApproved: true
   });
 });
+
+Recipe.create({
+  title: "tacos",
+  keyIngredients: ["onion", "tomato", "beef", "peppers"],
+  servings: 3,
+  prepTime: "20 minutes",
+  picture:
+    "https://images.unsplash.com/photo-1512838243191-e81e8f66f1fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+  instructions: "wash, slice and mix all the ingredients",
+  isApproved: true
+});
+});
+
+
+
+
+
+
+
