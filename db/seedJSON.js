e7c1f520c5b9a9a78d23ef6eacb3f6f5203a9b11
Liz Davidson <lizdavidson45@gmail.com>
2019-03-20 18:13:40 -0400

    revert search back to mongoDb query

--- db/seedJSON.js
+++ db/seedJSON.js
@@ -1,11 +1,10 @@
 const seedData = require("./jsonSeeds.json");
 const Recipe = require("../models/Recipe");
 
-
 // This code block is from the express to-do app in class.
 Recipe.remove({})
   .then(() => {
-    return Todo.collection.insert(seedData);
+    return Recipe.collection.insert(seedData);
   })
   .then(() => {
     process.exit();
