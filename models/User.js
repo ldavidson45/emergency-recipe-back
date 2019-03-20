5e5c5e4ab9db0c3aa4321ec50ba77d96a8cd0591
Liz Davidson <lizdavidson45@gmail.com>
2019-03-20 11:43:42 -0400

    add bcrypt to user model and set pre-save function to hash password

--- models/User.js
+++ models/User.js
@@ -1,9 +1,19 @@
 const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const User = new Schema({
   username: String,
   password: String
 });
 
+// hash user password before saving into database
+
+User.pre("save", function(next) {
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
+});
+
+// resource to use bcrypt: https://medium.com/@bhanushali.mahesh3/building-a-restful-crud-api-with-node-js-jwt-bcrypt-express-and-mongodb-4e1fb20b7f3d
 module.exports = mongoose.model("User", User);
