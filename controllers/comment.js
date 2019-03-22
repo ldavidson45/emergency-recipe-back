de1465b845f7e6d01ea31d3f91510ad8bb5ac072
Liz Davidson <lizdavidson45@gmail.com>
2019-03-22 11:38:06 -0400

    edit method to post a comment

--- controllers/comment.js
+++ controllers/comment.js
@@ -3,17 +3,25 @@ const router = express.Router();
 const mongoose = require("../models/Comment");
 const Comment = mongoose.model("Comment");
 const Recipe = mongoose.model("Recipe");
+const User = mongoose.model("User");
 
 router.post("/recipe/:id", (req, res) => {
-  Comment.create(req.body).then(comment => {
-    Recipe.findById({ _id: req.params.id }).then(recipe => {
-      recipe.comments.push(comment._id);
-      recipe.save(err => {
-        console.log(err);
+  User.findOne({ username: req.body.username })
+    .then(user => {
+      Comment.create({
+        content: req.body.content,
+        username: user._id
       });
-      res.json(recipe);
+    })
+    .then(comment => {
+      Recipe.findById({ _id: req.params.id }).then(recipe => {
+        recipe.comments.push(comment._id);
+        recipe.save(err => {
+          console.log(err);
+        });
+        res.json(recipe);
+      });
     });
-  });
 });
 
 router.delete("/:id", (req, res) => {
