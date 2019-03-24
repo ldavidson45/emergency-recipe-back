76de7393225129fc8ba8a2a888e9ea81dd948104
Liz Davidson <lizdavidson45@gmail.com>
2019-03-24 11:46:10 -0400

    move fuzzySearch function to new file

--- controllers/fuzzySearch.js
+++ controllers/fuzzySearch.js
@@ -1,18 +1,9 @@
 const express = require("express");
-const router = express.Router();
 const mongoose = require("../models/Recipe");
 const Recipe = mongoose.model("Recipe");
 const Fuse = require("fuse.js");
-const fuzzySearch = require("./fuzzySearch");
 
-router.post("/search", (req, res) => {
-  Recipe.find({ keyIngredients: { $all: req.body } }).then(recipes => {
-    res.json(recipes);
-  });
-});
-
-router.post("/", (req, res) => {
-  // find and list out filtered recipes
+function fuzzySearchrecipes() {
   Recipe.find()
     .then(recipes => {
       const options = {
@@ -80,54 +71,4 @@ router.post("/", (req, res) => {
     .catch(err => {
       console.log(err);
     });
-});
-
-router.get("/", (req, res) => {
-  // find and list out filtered recipes
-  Recipe.find().then(recipes => {
-    res.json(recipes);
-  });
-});
-
-router.post("/new", (req, res) => {
-  // create a new recipe in the database
-  Recipe.create(req.body)
-    .then(recipe => {
-      res.json(recipe);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-router.get("/:id", (req, res) => {
-  // show a single recipe
-  Recipe.findById(req.params.id)
-    .populate("comments")
-    .then(recipe => {
-      res.json(recipe);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-router.put("/:id", (req, res) => {
-  // update a recipe in the database
-  Recipe.findByIdAndUpdate(req.params.id, req.body).then(recipe => {
-    res.json(recipe);
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  // delete a recipe in the database
-  Recipe.findByIdAndDelete(req.params.id)
-    .then(recipe => {
-      res.json(recipe);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-module.exports = router;
+}
